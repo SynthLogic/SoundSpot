dd14157b6732f63fb77d08c5d8a923a8b3a06f8d
alcl2000 <amy.555.lewis@gmail.com>
2022-11-18 20:32:37 +0000

    Add: email.js API

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -10,3 +10,28 @@ function hideContact(){
         popUp.style.display = "none";
       }
 }
+
+/** Email.JS code */
+
+const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
+
+function sendMail(contactForm) {
+    modal.classList.remove("open");
+    overlay.classList.remove("open");
+    emailjs.send("gmail", "ms2", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function() {
+            alert("Thank you for getting in touch!");
+        },
+        function(error) {
+        console.log('FAILED...', error);
+        alert("There was an error with your message. Please try again.");
+        }
+    );
+    return false;  // To block from loading a new page
+}
