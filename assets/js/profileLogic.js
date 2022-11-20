f70b3b4a2fdc9bd3e16ed2dfe51e452df47fb805
Mike Avgeros <mike.avgeros@gmail.com>
2022-11-20 19:59:54 +0000

    remove global userData variable

--- assets/js/profileLogic.js
+++ assets/js/profileLogic.js
@@ -3,10 +3,8 @@ const email = document.querySelector('#email');
 const highestScore = document.querySelector('#high-score');
 const lastScore = document.querySelector('#last-score');
 
-let userData = {};
-
 if ('user' in sessionStorage) {
-  userData = JSON.parse(sessionStorage.getItem('user'));
+  const userData = JSON.parse(sessionStorage.getItem('user'));
   username.innerText = userData.username;
   email.innerText = userData.email;
   highestScore.innerText = userData.highestScore;
